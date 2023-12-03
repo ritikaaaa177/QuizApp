1baf9b72d08128187c92cd0cf627c64225c181e6
Ritika Kumar <kumarritika666@gmail.com>
2023-12-04 04:17:53 +1400

    login and register page add

---
+++ chatapp/src/App.js
@@ -0,0 +1,14 @@
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import SignUp from "./pages/SignUp";
+import("preline");
+
+function App() {
+  return (
+    <>
+      <SignUp />
+    </>
+  );
+}
+
+export default App;
